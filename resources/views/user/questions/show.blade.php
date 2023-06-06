62b4824cfd21f77fb6a80dab28b87dacb561b7bb
so2emma <osoemmanuel1969@gmail.com>
2023-06-06 22:37:28 +0100

    refactoring

--- resources/views/user/questions/show.blade.php
+++ resources/views/user/questions/show.blade.php
@@ -7,23 +7,28 @@
     <div class="container my-5">
         <div class="d-flex justify-content-between">
             <span class="h4  me-auto fw-bold">
-                 {{ $question->course->course_code.' '. $question->session->session_name.' session' }}
-                 @if ($question->userHasMadeSubmission(Auth::guard('web')->user(), $question))
-                 <button class="btn btn-dark-outline" disabled="disabled">Assignment Submitted</button>
-                 @else
-                    <a class="btn btn-primary" href="{{ route('user.submission.create', ['question' => $question->id ]) }}">Make A Submission</a>
-                 @endif
+                {{ $question->course->course_code . ' ' . $question->session->session_name . ' session' }}
             </span>
 
             <span>
-                <a href="{{ route('user.course.questions', ['course' => $question->course->id]) }}" class="btn btn-success">Back</a>
+                <a href="{{ route('user.course.questions', ['course' => $question->course->id]) }}"
+                    class="btn btn-success">Back</a>
             </span>
+        </div>
+        <div>
+            @if ($question->userHasMadeSubmission(Auth::guard('web')->user(), $question)->first())
+                <button class="btn btn-dark-outline" disabled="disabled">Assignment Submitted</button>
+                <a href="" class="btn btn-success">Grade Peers Submission</a>
+            @else
+                <a class="btn btn-primary" href="{{ route('user.submission.create', ['question' => $question->id]) }}">Make A
+                    Submission</a>
+            @endif
         </div>
     </div>
 
     <div class="container my-5">
         <div class="container">
-            <iframe src ="{{ Storage::url($question->path) }}" width="1000px" height="800px"></iframe>
+            <iframe src="{{ Storage::url($question->path) }}" width="1000px" height="800px"></iframe>
         </div>
     </div>
 @endsection
